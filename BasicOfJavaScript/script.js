32ff5574717d9485816db5c99740567e5e4f6f4a
amarrajanmishra <amar1998fzd@gmail.com>
2024-04-11 22:38:45 +0530

    JavaScript Basic Concept

---
+++ BasicOfJavaScript/script.js
@@ -0,0 +1,161 @@
+// console.log('Welcome to JavaScript')
+// console.error('Hello');
+// console.warn("Welcome")
+
+// Data Type
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+//  var a
+//  var b = null
+//  console.log(b)
+// var a = 20
+// var a = 50
+// console.log(a)
+// let b = 30
+// b = 50
+// const c = 40
+// console.log(c)
+
+// Non Primitive Data types
+// 1.Array
+// let arr = [1, 2, 3, 4, "hello", 2.4]
+// console.log(arr[4])
+// // 2.Object
+// let obj = {
+//     name : "Ajay",
+//     age : 34
+// }
+// console.log(obj.name)
+// obj.name = "Rajat"
+// obj["age"] =  36
+// console.log(obj["age"])
+// // Operators
+
+// Comparision Operator
+// ==
+// ===
+// let a = 20, b = "20"
+
+// if(a===b){
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+// All the operatoer are same just like python
+// control Structure
+// for(let i = 0; i < 10; i++){
+//     // Just Like all loop are same like java
+// }
+
+// switch is also same like java
+
+// 3 Ways To Assign a variable
+// var a = 20;
+// var a = 40;
+// a = 50;
+// let b = 30;
+// b = 40;
+// const c = 30;
+
+// Functions
+
+// function Function_Name(){
+//     // Function Definition
+// }
+
+// Function Without Parameter and without Returning Value
+
+// function Hi(){
+//     console.log("Hi")
+// }
+
+// Hi()
+
+// Function With Parameter and without Returning Value
+
+// function Hi(name){
+//     console.log("Hi", name)
+// }
+
+// Hi('Surya')
+
+// Function Without Parameter and with Returning Value
+
+// function sum(){
+//     let a = 20, b = 30
+//     let result = a + b
+//     return result
+// }
+
+// let res = sum()
+// console.log(res)
+
+// Function With Parameter and with Returning Value
+
+// function sum(a, b){
+//     let result = a + b
+//     return result
+// }
+
+// let res = sum(20, 30)
+// console.log(res)
+
+
+// Ecma Script
+// let a = "Ajay"
+
+// console.log("Hello" + " " + a + " " +"How R u")
+// console.log(`Hello ${a} How R U`)
+
+// Arrow Function
+
+// const Hi = () =>{
+//     console.log("Hi")
+// }
+// Hi()
+
+// const Hello = () => console.log("Hello")
+
+// Hello()
+
+// 
+// Anonymous Function
+// let a = function(){
+//     console.log("Hello")
+// }
+// a()
+
+
+// DOM (Document Object Model)
+// console.log(document.head)
+
+// Searching Methods (Selector)
+
+
+// console.log(document.getElementById("hi"))
+
+// let a = document.getElementById("hi")
+// a.innerHTML = "Hello Rajan"
+// console.log(a)
+
+// let a = document.getElementById("container")
+// a.innerHTML = `<h1>Hello Heading 1</h1> <H3>Hello Heading 2</H3>`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript-1/script.js
@@ -0,0 +1,114 @@
+// console.log('Hello world')
+// console.warn('Hello world')
+// console.error('Hello world')
+
+// alert('Hello world')
+
+// var a = 20
+
+// var a = 50
+// a = 60
+
+// console.log(a)
+
+// let b = 30
+
+// b = 60
+
+// console.log(b)
+
+// const c = 40
+
+
+// console.log(c)
+
+
+// Primitive Data Types
+// Number (int float double long)
+
+// let a = 20
+// let b = 20.5
+
+// console.log(a,b)
+// console.log(typeof(b))
+
+// // 2. String
+// let name = 'Ajay'
+// console.log(name)
+// console.log(typeof(name))
+
+// 3. Boolean(true and false)
+// let a = true
+// console.log(a)
+// console.log(typeof(a))
+
+// 4. undefined
+
+// let a
+// console.log(a)
+// console.log(typeof(a))
+
+// 5. Null
+// let a = null
+// console.log(a)
+// console.log(typeof(a))
+
+// Operators
+
+// let a = 20 
+// let b = 30
+// let result = a+b
+
+// Comparission operator
+
+// let a = 20
+// let b = '20'
+
+// console.log(a==b) // it compare only value
+// console.log(a===b) // it compare value as well as data types
+
+
+// Control Structure
+
+// if(condition){
+//     statement
+// }else if(condition){
+
+// }
+
+// switch(value){
+//     case 1:
+//         statement
+//         break;
+//     case 2:
+//         statement
+//         break;
+//     default:
+//         statement
+// }
+
+
+// loops
+// for loop
+
+// for(let i = 0; i < Condition;  i++){
+//     statement
+// }
+
+// while loop
+
+// while(condition){
+//     statement
+//     increment/decrement
+
+// }
+
+// do while loop
+
+// do{
+//     statement
+//     increment/decrement
+// }while(condition)
+
+
+
