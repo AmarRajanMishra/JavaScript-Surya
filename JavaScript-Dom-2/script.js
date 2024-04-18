eb353c3f9a28bd8c844675b18fde20fdea549e42
amarrajanmishra <amar1998fzd@gmail.com>
2024-04-18 23:54:54 +0530

    Dom

---
+++ JavaScript-Dom-2/script.js
@@ -0,0 +1,25 @@
+// console.log('Dom')
+
+// // Ecma Script => ES
+
+// console.log(document.body)
+
+
+// let a = document.getElementsByTagName('h1')
+// console.log(a)
+
+// let c = document.getElementsByClassName('Title')
+// console.log(c)
+
+// let b = document.getElementById('heading')
+// console.log(b)
+
+let a = document.getElementById('heading')
+a.textContent = 'Welcome To JavaScript Dom'
+a.style.backgroundColor ='red'
+
+console.log(a.textContent)
+
+let b = document.getElementById('')
+
+
